e20d91e75c05d78d9f7491f5856b2960bc23d934
ameso213 <142228988+ameso213@users.noreply.github.com>
2023-09-12 12:20:49 +0300

    how to use operations

---
+++ index.py
@@ -0,0 +1,50 @@
+#sum of even numbers less than 8
+p=2,4,6 #even numbers less than 8
+print(sum(p))
+print("the sum of even numbers lessthan 8 is"+str(sum(p)))
+
+#total number of fruits
+apples=6
+grapes=8
+oranges=5
+mangoes=2
+strawberries=3
+total_fruits=(apples+grapes+oranges+mangoes+strawberries)
+average=(total_fruits)/5
+print("the average is"+str(average))
+
+
+
+-5+8*6
+x=-5
+y=8
+z=6
+w=y*z
+print(x+w)
+
+(55+9)%9
+a=55
+b=9
+c=9
+d=(a+b)
+print(d%c)
+
+
+20+-3*5/8
+m=20
+n=-3*(5/8)
+print(m+n)
+
+5+15/3*2-8%3
+j=(5+(15/3)*2)-8
+k=3
+print(j%k)
+
+
+
+
+
+
+
+
+
